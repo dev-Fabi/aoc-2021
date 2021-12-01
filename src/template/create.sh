6e777581c46e60f676f30f0a89c4720ac61cfcb5
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-12-01 09:03:12 +0100

    Automate day/puzzle creation

---
+++ src/template/create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dayNum=$(date +'%-d')
+dayNumPadded=$(date +'%0d')
+
+packageName="day$dayNumPadded"
+filePathName="./$packageName/Day$dayNumPadded"
+mainFile="$filePathName.kt"
+
+# Make package
+mkdir "./$packageName"
+
+# Copy template file and create input files
+cp "./template/Day.tmp" $mainFile
+touch "$filePathName.txt"
+touch "${filePathName}_test.txt"
+
+# Replace placeholders in template
+sed -i '' "s/%PACKAGE/$packageName/g" $mainFile
+sed -i '' "s/%DAY/$dayNum/g" $mainFile
