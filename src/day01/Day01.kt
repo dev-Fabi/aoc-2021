e4d268c03cdf3f5a6b72b4307f2bdc412eb50f4c
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-12-01 08:23:09 +0100

    Day 01

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,22 +1,29 @@
 package day01
 
 import Utils
+import asInt
 
 fun main() {
     val utils = Utils(1)
 
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.asInt()
+            .zipWithNext()
+            .count { it.second > it.first }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asInt()
+            .windowed(3)
+            .map { it.sum() }
+            .zipWithNext()
+            .count { it.second > it.first }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = utils.readLines("test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = utils.readLines()
     println("Solution day ${utils.day}:")
