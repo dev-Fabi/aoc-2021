ed0e4bb7d5e046c04e80bc002a82ed1359e91bdb
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-12-04 17:14:54 +0100

    Day04 - Giant Squid

---
+++ src/day04/Day04.kt
@@ -0,0 +1,100 @@
+package day04
+
+import Utils
+import asInt
+
+data class Field(val value: Int, var marked: Boolean = false)
+class Board(init: List<String>) {
+    private var size: Int
+    private val rows: Array<Array<Field>>
+    private var markedRowCount: Array<Int>
+    private var markedColumnCount: Array<Int>
+
+    init {
+        val multiWhiteSpaceRegex = Regex(""" +""")
+        rows = init.map { it.trim().split(multiWhiteSpaceRegex).asInt() }
+            .map { line -> line.map { Field(it) }.toTypedArray() }.toTypedArray()
+        size = rows.size
+        markedColumnCount = Array(size) { 0 }
+        markedRowCount = Array(size) { 0 }
+    }
+
+    fun mark(value: Int): Boolean {
+        rows.forEachIndexed { rowIndex, row ->
+            row.withIndex().find { (_, field) -> !field.marked && field.value == value }?.let { (columnIndex, field) ->
+                field.marked = true
+                markedColumnCount[columnIndex]++
+                markedRowCount[rowIndex]++
+            }
+        }
+        return markedRowCount.any { it >= size } || markedColumnCount.any { it >= size }
+    }
+
+    fun sumUnchecked(): Int {
+        return rows.sumOf { row -> row.sumOf { if (!it.marked) it.value else 0 } }
+    }
+}
+
+fun main() {
+    val utils = Utils(4)
+
+    fun createGame(input: String): Pair<List<Int>, List<Board>> {
+        val sections = input.split("\n\n")
+        val numbers = sections.first().split(",").asInt()
+
+        val boards = sections.drop(1).map { Board(it.split("\n")) }
+
+        return numbers to boards
+    }
+
+    fun part1(numbers: List<Int>, boards: List<Board>): Int {
+        var winner: Board? = null
+        val numbersIterator = numbers.iterator()
+        var num: Int? = null
+        while (winner == null && numbersIterator.hasNext()) {
+            num = numbersIterator.next()
+            for (b in boards) {
+                if (b.mark(num)) {
+                    winner = b
+                    break;
+                }
+            }
+        }
+        if (num == null) error("No numbers drawn")
+        if (winner == null) error("No winner")
+
+        return winner.sumUnchecked() * num
+    }
+
+    fun part2(numbers: List<Int>, boards: List<Board>): Int {
+        var lastWinner: Board? = null
+        var winnerNumber: Int? = null
+
+        numbers.fold(boards) { acc, num ->
+            acc.filter { board ->
+                val won = board.mark(num)
+                if (won) {
+                    lastWinner = board
+                    winnerNumber = num
+                }
+
+                return@filter !won
+            }
+        }
+
+        if (lastWinner == null) error("No winner")
+
+        return lastWinner!!.sumUnchecked() * winnerNumber!!
+    }
+
+    // Test if implementation meets criteria from the description
+    val (testNumbers, testBoards) = createGame(utils.readFile("test"))
+    check(part1(testNumbers, testBoards) == 4512)
+    check(part2(testNumbers, testBoards) == 1924)
+
+    // Solve puzzle and print result
+    val (numbers, boards) = createGame(utils.readFile())
+    println("Solution day ${utils.day}:")
+    println("\tPart 1: " + part1(numbers, boards))
+    println("\tPart 2: " + part2(numbers, boards))
+}
